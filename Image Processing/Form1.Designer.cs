218014de35bb8b9d8d7c9b4ce0c64c053550b43b
Antonio Mancera Ubaldo Jr. <142708326+JunSayke@users.noreply.github.com>
2024-11-04 11:20:09 +0800

    Add project files.

---
+++ Image Processing/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Image_Processing
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            fileToolStripMenuItem = new ToolStripMenuItem();
+            openToolStripMenuItem = new ToolStripMenuItem();
+            saveToolStripMenuItem = new ToolStripMenuItem();
+            dIPToolStripMenuItem = new ToolStripMenuItem();
+            pixelCopyToolStripMenuItem = new ToolStripMenuItem();
+            grayScaleToolStripMenuItem = new ToolStripMenuItem();
+            inversionToolStripMenuItem = new ToolStripMenuItem();
+            mirrorHorizontalToolStripMenuItem = new ToolStripMenuItem();
+            mirronVerticalToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1 = new MenuStrip();
+            saveFileDialog1 = new SaveFileDialog();
+            openFileDialog1 = new OpenFileDialog();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(37, 65);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(338, 333);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(425, 65);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(338, 333);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 2;
+            pictureBox2.TabStop = false;
+            // 
+            // fileToolStripMenuItem
+            // 
+            fileToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { openToolStripMenuItem, saveToolStripMenuItem });
+            fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            fileToolStripMenuItem.Size = new Size(37, 20);
+            fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            openToolStripMenuItem.Name = "openToolStripMenuItem";
+            openToolStripMenuItem.Size = new Size(103, 22);
+            openToolStripMenuItem.Text = "Open";
+            openToolStripMenuItem.Click += openToolStripMenuItem_Click;
+            // 
+            // saveToolStripMenuItem
+            // 
+            saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            saveToolStripMenuItem.Size = new Size(103, 22);
+            saveToolStripMenuItem.Text = "Save";
+            saveToolStripMenuItem.Click += saveToolStripMenuItem_Click;
+            // 
+            // dIPToolStripMenuItem
+            // 
+            dIPToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { pixelCopyToolStripMenuItem, grayScaleToolStripMenuItem, inversionToolStripMenuItem, mirrorHorizontalToolStripMenuItem, mirronVerticalToolStripMenuItem });
+            dIPToolStripMenuItem.Name = "dIPToolStripMenuItem";
+            dIPToolStripMenuItem.Size = new Size(37, 20);
+            dIPToolStripMenuItem.Text = "DIP";
+            // 
+            // pixelCopyToolStripMenuItem
+            // 
+            pixelCopyToolStripMenuItem.Name = "pixelCopyToolStripMenuItem";
+            pixelCopyToolStripMenuItem.Size = new Size(180, 22);
+            pixelCopyToolStripMenuItem.Text = "Pixel Copy";
+            pixelCopyToolStripMenuItem.Click += pixelCopyToolStripMenuItem_Click;
+            // 
+            // grayScaleToolStripMenuItem
+            // 
+            grayScaleToolStripMenuItem.Name = "grayScaleToolStripMenuItem";
+            grayScaleToolStripMenuItem.Size = new Size(180, 22);
+            grayScaleToolStripMenuItem.Text = "Greyscaling";
+            grayScaleToolStripMenuItem.Click += grayScaleToolStripMenuItem_Click;
+            // 
+            // inversionToolStripMenuItem
+            // 
+            inversionToolStripMenuItem.Name = "inversionToolStripMenuItem";
+            inversionToolStripMenuItem.Size = new Size(180, 22);
+            inversionToolStripMenuItem.Text = "Inversion";
+            inversionToolStripMenuItem.Click += inversionToolStripMenuItem_Click;
+            // 
+            // mirrorHorizontalToolStripMenuItem
+            // 
+            mirrorHorizontalToolStripMenuItem.Name = "mirrorHorizontalToolStripMenuItem";
+            mirrorHorizontalToolStripMenuItem.Size = new Size(180, 22);
+            mirrorHorizontalToolStripMenuItem.Text = "Mirror Horizontal";
+            // 
+            // mirronVerticalToolStripMenuItem
+            // 
+            mirronVerticalToolStripMenuItem.Name = "mirronVerticalToolStripMenuItem";
+            mirronVerticalToolStripMenuItem.Size = new Size(180, 22);
+            mirronVerticalToolStripMenuItem.Text = "Mirron Vertical";
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { fileToolStripMenuItem, dIPToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            menuStrip1.ItemClicked += menuStrip1_ItemClicked;
+            // 
+            // saveFileDialog1
+            // 
+            saveFileDialog1.FileOk += saveFileDialog1_FileOk;
+            // 
+            // openFileDialog1
+            // 
+            openFileDialog1.FileName = "openFileDialog1";
+            openFileDialog1.FileOk += openFileDialog1_FileOk_1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private ToolStripMenuItem fileToolStripMenuItem;
+        private ToolStripMenuItem openToolStripMenuItem;
+        private ToolStripMenuItem saveToolStripMenuItem;
+        private ToolStripMenuItem dIPToolStripMenuItem;
+        private ToolStripMenuItem pixelCopyToolStripMenuItem;
+        private ToolStripMenuItem grayScaleToolStripMenuItem;
+        private ToolStripMenuItem inversionToolStripMenuItem;
+        private ToolStripMenuItem mirrorHorizontalToolStripMenuItem;
+        private ToolStripMenuItem mirronVerticalToolStripMenuItem;
+        private MenuStrip menuStrip1;
+        private SaveFileDialog saveFileDialog1;
+        private OpenFileDialog openFileDialog1;
+    }
+}
---
+++ Image Processing/Form1.cs
@@ -0,0 +1,92 @@
+namespace Image_Processing
+{
+    public partial class Form1 : Form
+    {
+        Bitmap loaded, processed;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk_1(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            loaded = new Bitmap(openFileDialog1.FileName);
+            pictureBox1.Image = loaded;
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void pixelCopyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            processed = new Bitmap(loaded.Width, loaded.Height);
+            Color pixel;
+            for (int i = 0; i < loaded.Width; i++)
+            {
+                for (int j = 0; j < loaded.Height; j++)
+                {
+                    pixel = loaded.GetPixel(i, j);
+                    processed.SetPixel(i, j, pixel);
+                }
+            }
+            pictureBox2.Image = processed;
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+        }
+
+        private void saveFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            processed.Save(saveFileDialog1.FileName);
+        }
+
+        private void grayScaleToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            processed = new Bitmap(loaded.Width, loaded.Height);
+            Color pixel;
+            int ave;
+            for (int i = 0; i < loaded.Width; i++)
+            {
+                for (int j = 0; j < loaded.Height; j++)
+                {
+                    pixel = loaded.GetPixel(i, j);
+                    ave = (int)(pixel.R + pixel.G + pixel.B) / 3;
+                    Color gray = Color.FromArgb(ave, ave, ave);
+                    processed.SetPixel(i, j, gray);
+                }
+            }
+            pictureBox2.Image = processed;
+        }
+
+        private void inversionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            processed = new Bitmap(loaded.Width, loaded.Height);
+            Color pixel;
+            for (int i = 0; i < loaded.Width; i++)
+            {
+                for (int j = 0; j < loaded.Height; j++)
+                {
+                    pixel = loaded.GetPixel(i, j);
+                    Color invert = Color.FromArgb(255 - pixel.R, 255 - pixel.G, 255 - pixel.B);
+                    processed.SetPixel(i, j, invert);
+                }
+            }
+            pictureBox2.Image = processed;
+        }
+    }
+}
